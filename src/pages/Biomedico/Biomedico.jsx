c5c8881210464089590399d7e3d1a6052ae882fc
DanielSaavC <134180560+DanielSaavC@users.noreply.github.com>
2025-10-14 15:29:00 -0400

    c

--- src/pages/Biomedico/Biomedico.jsx
+++ src/pages/Biomedico/Biomedico.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link} from "react-router-dom";
 import "../../App.css";
 import Header from "../../components/Header";
 
