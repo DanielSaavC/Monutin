ad7e073949cb5256962cdb0aa44ad19a339622dc
DanielSaavC <134180560+DanielSaavC@users.noreply.github.com>
2025-10-14 15:52:42 -0400

    n

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // src/api.js
-export const API_URL = "http://localhost:4000";
+export const API_URL = "http://10.192.197.65:4000";
 
 export async function login(nickname, password) {
   const res = await fetch(`${API_URL}/login`, {
